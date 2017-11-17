f5364b94ce079acfa63fd94f60e223c188fbdbd9
Charlie Yang <charlesy@stanford.edu>
2017-11-17 00:32:22 -0800

    feat: finished first page. go vanilla JS!

---
+++ assets/js/home.js
@@ -0,0 +1,47 @@
+function isScrolledIntoView(elem, index) {
+    var percent = 0.25;
+    var buffer = window.innerHeight * percent;
+    var docViewTop = window.scrollY;
+    var docViewBottom = docViewTop + window.innerHeight;
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    if (index === 0) {
+        // console.log(docViewTop, docViewBottom, elemTop, elemBottom);
+        // console.log(elemBottom <= docViewBottom, elemTop >= docViewTop);
+    }
+    return (
+        elemBottom + buffer <= docViewBottom && elemTop - buffer >= docViewTop
+    );
+}
+
+function resetHoverClasses() {
+    if (window.innerWidth > 600) {
+        $(".main-icon").removeClass("hover");
+        return true;
+    }
+    return false;
+}
+$(document).ready(function() {
+    // console.log("READY");
+    $(window).resize(function() {
+        resetHoverClasses();
+    });
+
+    $(window).scroll(function() {
+        if (resetHoverClasses()) return;
+
+        $(".main-icon").each(function(index, elem) {
+            var $elem = $(elem);
+            if (isScrolledIntoView(elem, index)) {
+                if (!$elem.hasClass("hover")) {
+                    // console.log("add");
+                    $elem.addClass("hover");
+                }
+            } else {
+                $elem.removeClass("hover");
+            }
+        });
+    });
+});
